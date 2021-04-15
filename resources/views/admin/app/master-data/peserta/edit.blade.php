e48f0d78eaa4c06d4f7a098f0a67e91d23fdf54e
rheznendra <rheznendra@gmail.com>
2021-04-16 03:30:31 +0800

    Fix edit data peserta admin.

--- resources/views/admin/app/master-data/peserta/edit.blade.php
+++ resources/views/admin/app/master-data/peserta/edit.blade.php
@@ -48,7 +48,7 @@
                         </div>
                         @foreach ($data->Anggota()->get() as $index => $item)
                         <div class="form-group row mb-4">
-                            {!! Form::label('nim_anggota[]', 'NIM Anggota ' . $index+1, ['class' => 'col-form-label text-md-right col-12 col-md-3 col-lg-3']) !!}
+                            {!! Form::label('nim_anggota[]', 'NIM Anggota ' . ($index+1), ['class' => 'col-form-label text-md-right col-12 col-md-3 col-lg-3']) !!}
                             <div class="col-sm-7 col-md-7 col-12">
                                 {!! Form::text('nim_anggota[]', old('nim_anggota.' . $index) ?? $item->nim, ['class' => 'form-control' . ($errors->has('nim_anggota.' . $index) ? ' is-invalid' : null), 'autocomplete' => 'off']) !!}
                                 @error('nim_anggota.' . $index)
@@ -59,7 +59,7 @@
                             </div>
                         </div>
                         <div class="form-group row mb-4 form-separator">
-                            {!! Form::label('nama_anggota[]', 'Nama Anggota ' . $index+1, ['class' => 'col-form-label text-md-right col-12 col-md-3 col-lg-3']) !!}
+                            {!! Form::label('nama_anggota[]', 'Nama Anggota ' . ($index+1), ['class' => 'col-form-label text-md-right col-12 col-md-3 col-lg-3']) !!}
                             <div class="col-sm-7 col-md-7 col-12">
                                 {!! Form::text('nama_anggota[]', old('nama_anggota.' . $index) ?? $item->nama, ['class' => 'form-control' . ($errors->has('nama_anggota.' . $index) ? ' is-invalid' : null), 'autocomplete' => 'off']) !!}
                                 @error('nama_anggota.' . $index)
